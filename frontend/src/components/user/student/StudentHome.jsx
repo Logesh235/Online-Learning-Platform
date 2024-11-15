bd9c340474ce2b1493c19e92d92481ed8d0763e5
Logesh <logeshsmart235@gmail.com>
2024-11-15 18:21:18 +0530

    add Online-Learning-Platform master

---
+++ frontend/src/components/user/student/StudentHome.jsx
@@ -0,0 +1,15 @@
+import AllCourses from '../../common/AllCourses'
+import { Container } from 'react-bootstrap'
+
+const StudentHome = () => {
+
+   return (
+      <>
+         <Container>
+            <AllCourses />
+         </Container>
+      </>
+   )
+}
+
+export default StudentHome
